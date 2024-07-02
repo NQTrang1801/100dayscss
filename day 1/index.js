3be5cad4efe61d3c2f06b8aae16c939f26551204
NQTrang1801 <trangnguyen180103@gmail.com>
2024-07-02 19:14:03 +0700

    added: day 1

---
+++ day 1/index.js
@@ -0,0 +1,4 @@
+document.querySelector('.container').addEventListener("click", ()=> {
+    document.querySelector('.container').classList.toggle("active")
+    document.querySelector('.container').classList.toggle("no-active")
+})
